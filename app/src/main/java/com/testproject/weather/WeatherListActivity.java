f520dc6f990fcc318ad326abc90c9c0e02ac4c2f
olena1911 <helenboyko@ukr.net>
2018-08-23 14:16:37 +0300

    Add weather item layout and weather list

---
+++ app/src/main/java/com/testproject/weather/WeatherListActivity.java
@@ -0,0 +1,21 @@
+package com.testproject.weather;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class WeatherListActivity extends Activity {
+
+    private RecyclerView mWeatherRecyclerView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_list);
+
+        mWeatherRecyclerView = (RecyclerView) findViewById(R.id.list_weather);
+        mWeatherRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
