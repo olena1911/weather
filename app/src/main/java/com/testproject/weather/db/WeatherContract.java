cb4e4558b927c919c9770025c4a6923dba7cab8e
olena1911 <helenboyko@ukr.net>
2018-08-23 21:59:37 +0300

    Add WeatherContract class

---
+++ app/src/main/java/com/testproject/weather/db/WeatherContract.java
@@ -0,0 +1,43 @@
+package com.testproject.weather.db;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class WeatherContract {
+
+    public static final String CONTENT_AUTHORITY = "com.testproject.weather";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_WEATHER = "weather";
+
+    private WeatherContract() {
+    }
+
+    public static class Weather implements BaseColumns {
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_WEATHER);
+
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
+
+        public static final String TABLE_NAME = "weather";
+
+        public final static String _ID = BaseColumns._ID;
+
+        public static final String COLUMN_CITY_NAME = "city_name";
+        public static final String COLUMN_CITY_ID = "city_id";
+        public static final String COLUMN_TIME = "time";
+        public static final String COLUMN_LONGITUDE = "longitude";
+        public static final String COLUMN_LATITUDE = "latitude";
+        public static final String COLUMN_TEMPERATURE = "temperature";
+        public static final String COLUMN_PRESSURE = "pressure";
+        public static final String COLUMN_HUMIDITY = "humidity";
+        public static final String COLUMN_MIN_TEMPERATURE = "min_temperature";
+        public static final String COLUMN_MAX_TEMPERATURE = "max_temperature";
+        public static final String COLUMN_WIND_SPEED = "wind_speed";
+        public static final String COLUMN_WIND_DEGREE = "wind_degree";
+        public static final String COLUMN_CLOUDINESS = "cloudiness";
+    }
+}
