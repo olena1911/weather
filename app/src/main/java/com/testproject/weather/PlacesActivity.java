cc9727d2e602389b0456233d2a71233936d57e7d
olena1911 <helenboyko@ukr.net>
2018-08-24 20:40:35 +0300

    Set cursor adapter to places recycler view

--- app/src/main/java/com/testproject/weather/PlacesActivity.java
+++ app/src/main/java/com/testproject/weather/PlacesActivity.java
@@ -2,12 +2,27 @@ package com.testproject.weather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class PlacesActivity extends AppCompatActivity {
+
+    private RecyclerView mPlacesRecyclerView;
+    private PlacesCursorAdapter mPlacesCursorAdapter;
+
+    private static final int PLACES_LOADER_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_places);
+
+        mPlacesRecyclerView = (RecyclerView) findViewById(R.id.list_places);
+        mPlacesRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mPlacesCursorAdapter = new PlacesCursorAdapter(this, null);
+        mPlacesRecyclerView.setAdapter(mPlacesCursorAdapter);
+
+        //getLoaderManager().initLoader(PLACES_LOADER_ID, null, this);
     }
 }
