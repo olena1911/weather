3d50ba2d12ff1ac2c805da2f2d0bc50b03bc1b11
olena1911 <helenboyko@ukr.net>
2018-08-29 12:09:47 +0300

    Add and remove marker on the map

--- app/src/main/java/com/testproject/weather/MapActivity.java
+++ app/src/main/java/com/testproject/weather/MapActivity.java
@@ -1,47 +1,62 @@
 package com.testproject.weather;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
 
     private GoogleMap mMap;
+    private Marker currentMarker;
+    private LatLng currentMarkerPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        if (savedInstanceState != null) {
+            currentMarkerPosition = savedInstanceState.getParcelable("currentMarkerPosition");
+        }
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (currentMarker != null) {
+            outState.putParcelable("currentMarkerPosition", currentMarkerPosition);
+        }
+    }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        if (currentMarkerPosition != null) {
+            currentMarker = mMap.addMarker(new MarkerOptions().position(currentMarkerPosition));
+        }
+        mMap.setOnMapClickListener(this);
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    @Override
+    public void onMapClick(LatLng latLng) {
+        if (currentMarker != null) {
+            currentMarker.remove();
+            currentMarker = null;
+            currentMarkerPosition = null;
+        } else {
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+            currentMarker = mMap.addMarker(new MarkerOptions().position(latLng));
+            currentMarkerPosition = currentMarker.getPosition();
+        }
     }
 
 }
